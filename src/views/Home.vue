d91ab22680490da94a218ad53e74093ae2153bfc
FBourhis <frederic.bourhis@outlook.com>
2020-09-28 16:14:10 +0200

    Button & Icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, itaque! Dignissimos ut voluptatem quis cumque id mollitia nesciunt facere, magnam dolore blanditiis beatae laboriosam fuga ullam saepe natus consectetur accusantium?</p>
-    <p class="pink  lighten-4 red--text text--darken-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, minus pariatur earum cumque quaerat qui fugit iste voluptatum maiores quod, ad nostrum. Praesentium rem, quas quo consequuntur dignissimos libero voluptate.</p>
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">Smaller Display</h4>
-    <p class="headline">This is a headline</p>
-    <p class="subheading font-weight-bold">This is a subheading font-weight-bold</p>
-    <p class="caption text-uppercase">This is a caption upper-case</p>
+    <v-btn class="pink white--text">Click me !</v-btn>
+    <v-btn text dark color="pink">Click me !</v-btn>
+    <v-btn depressed color="pink">
+      <v-icon left medium>mdi-email</v-icon>
+      <span>EMAIL ME</span>
+    </v-btn>
+
+    <v-btn small depressed color="pink">
+      <v-icon left small>mdi-email</v-icon>
+      <span>EMAIL ME</span>
+    </v-btn>
+
+    <v-btn fab dark>
+      <v-icon color="pink">mdi-heart</v-icon>
+    </v-btn>
+    
+
   </div>
 </template>
 
@@ -16,4 +26,11 @@
 export default {
   
 }
+
 </script>
+
+<style scoped>
+button{
+  margin: 10px;
+}
+</style>
