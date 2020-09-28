fd2370eead21dc0618da9337e18ce2cf337c07d4
FBourhis <frederic.bourhis@outlook.com>
2020-09-28 12:45:59 +0200

    text & colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, itaque! Dignissimos ut voluptatem quis cumque id mollitia nesciunt facere, magnam dolore blanditiis beatae laboriosam fuga ullam saepe natus consectetur accusantium?</p>
+    <p class="pink  lighten-4 red--text text--darken-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, minus pariatur earum cumque quaerat qui fugit iste voluptatum maiores quod, ad nostrum. Praesentium rem, quas quo consequuntur dignissimos libero voluptate.</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller Display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-bold">This is a subheading font-weight-bold</p>
+    <p class="caption text-uppercase">This is a caption upper-case</p>
   </div>
 </template>
 
