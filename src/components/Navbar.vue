ae09dac9e1d7ae6297f64f845f6c7e54ae904d40
FBourhis <frederic.bourhis@outlook.com>
2020-09-29 09:47:47 +0200

    Lists

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,8 +13,17 @@
       </v-btn>
     </v-app-bar>
 
-    <v-navigation-drawer app v-model="drawer" class="indigo">
-      <p>Test</p>
+    <v-navigation-drawer app v-model="drawer" class="warning">
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-action>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
   </nav>
@@ -25,6 +34,11 @@ export default {
   data(){
     return {
       drawer: false,
+      links: [
+         { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/' },
+         { icon: 'mdi-folder', text: 'My Projects', route: '/projects' },
+         { icon: 'mdi-account', text: 'Team', route: '/team' },
+      ],
     }
   }
 }
