a9a27ee114157f68d3d90f5f8e618d6426012bec
FBourhis <frederic.bourhis@outlook.com>
2020-09-29 16:42:28 +0200

    Avatars

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,17 @@
     </v-app-bar>
 
     <v-navigation-drawer app v-model="drawer" class="primary">
-      <v-list dark>
+      <v-layout column align-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img src="/avatar-3.png">
+          </v-avatar>
+          <p class="text-center white--text subheading mt-1" color="white">
+            Chun Li
+          </p>
+        </v-flex>
+      </v-layout>
+      <v-list>
         <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-item-action>
             <v-icon>{{ link.icon }}</v-icon>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="team">
+  <div light class="team">
     <h1 class="subtitle-1 grey--text">Team</h1>
 
     <v-container class="my-5">
       
       <v-layout row>
         <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
-          <v-card flat class="text-center ma-3">
+          <v-card text class="text-center ma-3">
             <v-responsive class="pt-4">
-              image goes here
+              <v-avatar size="100" class="grey lighten-2">
+                <img :src="person.avatar">
+              </v-avatar>
             </v-responsive>
             <v-card-text>
               <div class="subheading">{{ person.name }}</div>
               <div color="grey">{{ person.role }}</div>
             </v-card-text>
             <v-card-actions>
-              <v-btn flat color="grey">
+              <v-btn text color="grey">
                 <v-icon small left>mdi-message</v-icon>
                 <span>Message</span>
               </v-btn>
@@ -35,11 +37,11 @@ export default {
   data () {
     return {
       team: [
-        { name: 'The Net Ninja', role: 'Web developer' },
-        { name: 'Ryu', role: 'Graphic designer' },
-        { name: 'Chun Li', role: 'Web developer' },
-        { name: 'Gouken', role: 'Social media maverick' },
-        { name: 'Yoshi', role: 'Sales guru'}
+        { name: 'The Net Ninja', role: 'Web developer', avatar: '/avatar-1.png'},
+        { name: 'Ryu', role: 'Graphic designer', avatar: '/avatar-2.png'},
+        { name: 'Chun Li', role: 'Web developer', avatar: '/avatar-3.png'},
+        { name: 'Gouken', role: 'Social media maverick', avatar: '/avatar-4.png'},
+        { name: 'Yoshi', role: 'Sales guru', avatar: '/avatar-5.png'}
       ]
     }
   },
