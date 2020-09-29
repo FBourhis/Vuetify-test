a1ce3e507660caab47be0f506ff84c0a7d7e2b43
FBourhis <frederic.bourhis@outlook.com>
2020-09-29 11:20:28 +0200

    Grid System 2

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,30 +3,27 @@
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
     
     <v-container class="my-5">
-      
-      <v-layout row wrap>
-        <v-flex xs12 md6>
-          <v-btn outlined block color="primary">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outlined block color="primary">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outlined block color="primary">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outlined block color="primary">1</v-btn>
-        </v-flex>
-      </v-layout>
-
-      <v-layout row wrap justify-space-around>
-        <v-flex xs4 md3>
-          <v-btn outlined block color="success">Button</v-btn>
-        </v-flex>
-        <v-flex xs4 md3>
-          <v-btn outlined block color="success">Button</v-btn>
-        </v-flex>
-      </v-layout>
+        
+        <v-card flat class="pa-3">
+          <v-layout row wrap>
+            <v-flex xs12 md6>
+              <div class="caption grey--text">Project title</div>
+              <div>Create a new website</div>
+            </v-flex>
+            <v-flex xs6 sm4 md2>
+              <div class="caption grey--text">Person</div>
+              <div>The Net Ninja</div>
+            </v-flex>
+            <v-flex xs6 sm4 md2>
+              <div class="caption grey--text">Due By</div>
+              <div>2nd Feb 2020</div>
+            </v-flex>
+            <v-flex xs2 sm4 md2>
+              <div class="caption grey--text">Status</div>
+              <div>Ongoing</div>
+            </v-flex>
+          </v-layout>
+        </v-card>
 
     </v-container>
 
